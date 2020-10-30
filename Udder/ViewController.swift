a6f06a3f1397a878275afc74eaaa6ca68aa08252
Anastasiya Ulitina <anastulitina@gmail.com>
2020-10-30 21:16:09 +0300

    Initial Commit

---
+++ Udder/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Udder
+//
+//  Created by Анастасия Улитина on 30.10.2020.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
